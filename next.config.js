5b5179b7c8980b5733e17ea88e43fb85210a7045
Usman Khan <usman918khan@gmail.com>
2025-06-07 21:51:38 +0500

    testing build

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  basePath: '/AiTechBlog',
+};
+
+module.exports = nextConfig;
