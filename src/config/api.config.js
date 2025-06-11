641352e92896c664c6d7cb7ad956b41f5a90379a
Usman Khan <usman918khan@gmail.com>
2025-06-11 12:41:04 +0500

    Move API key to environment variables for security

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,2 +1,2 @@
-export const NEWS_API_KEY = '58cdbfb6623b4ba3ac48f8fbab9d3de4';
+export const NEWS_API_KEY = process.env.NEXT_PUBLIC_NEWS_API_KEY;
 export const NEWS_API_BASE_URL = 'https://newsapi.org/v2';
